6ffb888002714cff24a3f376c04a43add55ceccb
Jeanfranco <112358285+jean03f@users.noreply.github.com>
2024-08-11 18:45:23 -0400

    comments added

--- script.js
+++ script.js
@@ -10,13 +10,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const postElement = document.createElement('div');
 
+        // It provides a card layout to separe each post; providing a buttom and top margin of 3
         postElement.classList.add("card", "card-body", "mb-3", "mt-3")
 
+        // Each element will margins in the right and left too
         postElement.style.marginRight = "2rem";
         postElement.style.marginLeft = "2rem";
 
 
-
+        // For each iteration, the data from the json file will be added to the blogList 
+        // Each post will have a title and a description under it 
         postElement.innerHTML = `
         <br> <h1><strong>${post.Title}</strong></h1>
         <br> 
@@ -26,5 +29,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     })
 
+    //If at any point the fetch fails, it will output it in the console
     .catch(error => console.error('Error loading blog posts:', error));
     });
